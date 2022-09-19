9322b85f16c33f36224996d36b2d08f6fb27f99a
Venkatesh7981615724 <99963595+Venkatesh7981615724@users.noreply.github.com>
2022-09-19 07:27:09 +0530

    Delete problem3.py

--- Exersice1/problem3.py
+++
@@ -1,9 +0,0 @@
-# We have the name and seat numbers of a student given below as two tuples. With this given data, print the students' names
-# and their assigned seat numbers in a single line using the appropriate data type.
-# name = ('Shaun', 'Ron', 'Michael')
-# seat_numbers = ('Shaun', 'Ron', 'Michael')
-
-name = ('Shaun', 'Ron', 'Michael')
-seat_numbers = (101, 102, 103)
-result=dict(zip(name,seat_numbers))
-print(result)
